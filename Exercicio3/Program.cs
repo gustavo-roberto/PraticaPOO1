f6f2b9f19ef379d3e8eaa8b3d7182699f3ac464c
Gustavo Roberto <gustavo.rmattos@gmail.com>
2021-04-13 20:04:05 -0300

    commit pendente

--- Exercicio3/Program.cs
+++ Exercicio3/Program.cs
@@ -1,10 +1,33 @@
-﻿
+﻿using Exercicio3.Modelos;
+
 namespace Exercicio3
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Elevador elevador = new Elevador(4, 4);
+
+            elevador.Descer();
+            elevador.Subir();
+            elevador.Subir();
+            elevador.Subir();
+            elevador.Subir();
+            elevador.Subir();
+
+            elevador.Descer();
+            elevador.MostrarAndarAtual();
+
+            elevador.Sair();
+
+            elevador.Entrar();
+            elevador.Entrar();
+            elevador.Entrar();
+            elevador.Entrar();
+            elevador.Entrar();
+
+            elevador.Sair();
+            elevador.MostrarNumAtualOcupantes();
         }
     }
 }
